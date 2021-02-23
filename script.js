e85276ef737e505956ae870f6f936d859a198496
StevenGrobe <steven.grobe@le-campus-numerique.fr>
2021-02-23 14:38:56 +0100

    exo-2.2 WIP

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log(`${cities.length} communes chargées`);
+
+let newArray = cities.filter((element) => element.population > 300000)
+newArray.sort(function(a,b) {
+    return b.population - a.population;
+});
+console.log(newArray)
+
+function getCitiesByDept (codePostal) {
+    let codePostalArray = cities.filter((element) => element.codeDepartement == codePostal)
+    for (let i = 0; i < codePostalArray.length; i++) {
+        console.log(codePostalArray[i].nom);
+    }
+};
+
+getCitiesByDept(74);
+
