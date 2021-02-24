45f75abbc57409da2497e4960b0e7878cb29ad94
StevenGrobe <grobesteven@gmail.com>
2021-02-24 16:45:19 +0100

    exo-4 list wip

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log(`${cities.length} communes chargées`);
 
 function ready(callback) {
-  if (document.readyState != 'loading'){
+  if (document.readyState != 'loading') {
     callback();
   } else {
     document.addEventListener('DOMContentLoaded', callback);
@@ -9,10 +9,32 @@ function ready(callback) {
 }
 
 // insérer votre code ci-dessous
+const codeDept = '38';
+
+function getCitiesByDept(codeDept) {
+  const CitiesByDeptArray = cities.filter((city) => city.codeDepartement === codeDept);
+  return CitiesByDeptArray;
+}
+
+function displayCity(city) {
+  return `${city.nom}`;
+}
+
+function getArrayCities(codeDept) {
+  const arrayNul = getCitiesByDept(codeDept).map(city => displayCity(city))
+  return arrayNul;
+}
+
+const arrayNul = getArrayCities(codeDept)
+console.log(arrayNul)
+
 function main() {
-  const el = document.createElement('div');
-  el.innerHTML = 'Je suis un div';
-  document.body.appendChild(el);
+  for (let i = 0; i < arrayNul.length; i++) {
+    const el = document.createElement('option');
+    el.innerHTML = arrayNul[i];
+    document.body.appendChild(el);
+  }
 }
 
 ready(main);
+
