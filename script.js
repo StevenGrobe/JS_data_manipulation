fb82444aad49a596d98735353bc921e0c98a705e
StevenGrobe <grobesteven@gmail.com>
2021-02-24 13:07:25 +0100

    exo-1 RFV

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log(`${cities.length} communes chargées`);
 
-let resultArray = []; // Tableau vide
+const resultArray = []; // Tableau vide
 
 for (let i = 0; i < cities.length; i++) { // Compare la population et push dans tableau vide
     if (cities[i].population > 300000) {
@@ -8,8 +8,8 @@ for (let i = 0; i < cities.length; i++) { // Compare la population et push dans 
     }
 }
 
-resultArray.sort(function(a, b)  { // Range de façon décroissante la population
-    return b.population - a.population;
+resultArray.sort(function(prev, curr)  { // Range de façon décroissante la population
+    return curr.population - prev.population;
 });
 
 for (let j = 0; j < resultArray.length; j++) { // Affichage de mon nouveau tableau
